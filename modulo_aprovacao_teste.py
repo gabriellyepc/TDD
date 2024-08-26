4849acbaa4b4d33488bd248499761a769f5dfdd2
Gabrielly Chacara <160495052+gabriellyepc@users.noreply.github.com>
2024-08-26 08:34:55 -0300

    Add files via upload

---
+++ modulo_aprovacao_teste.py
@@ -0,0 +1,16 @@
+import unittest
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+import modulo_aprovacao
+
+teste_todos_passam
+
+
+
+
+
+unittest.main(verbosity=2)
